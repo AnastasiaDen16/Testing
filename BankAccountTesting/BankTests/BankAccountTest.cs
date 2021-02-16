abdde0d6a65337537d594cad1b99fc93ccf78a12
AnastasiaDen16 <anastdenisova16@gmail.com>
2021-02-16 10:25:47 +0300

    Add

---
+++ BankAccountTesting/BankTests/BankAccountTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountTesting;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
