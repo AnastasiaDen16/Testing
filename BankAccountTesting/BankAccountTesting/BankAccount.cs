43eceb1239a64b26c937422dfbdfb36b72528fc5
AnastasiaDen16 <anastdenisova16@gmail.com>
2021-02-16 10:53:14 +0300

    Finish Test

--- BankAccountTesting/BankAccountTesting/BankAccount.cs
+++ BankAccountTesting/BankAccountTesting/BankAccount.cs
@@ -6,6 +6,8 @@ namespace BankAccountTesting
     {
         private readonly string m_customerName;
         private double m_balance;
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
 
         private BankAccount() { }
 
@@ -29,12 +31,12 @@ namespace BankAccountTesting
         {
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
             m_balance -= amount; // intentionally incorrect code
